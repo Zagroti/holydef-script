a7e2bffdb041a260a2d0e1edaa31869625b464b7
yaser <yaser.darzi@gmail.com>
2018-09-17 10:04:30 +0430

    add constants

---
+++ app/Inside/Constants.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Inside;
+
+class Constants
+{
+
+    Const LOGIN_TYPE_SMS = "login_with_sms";
+    Const LOGIN_TYPE_CALL = "login_with_call";
+    Const LOGIN_TYPE_EMAIL = "login_with_call";
+
+    //DataBase
+    const USERS_DB = 'users';
+    const USERS_LOGIN_TOKEN_DB = 'users_login_token';
+    const USERS_LOGIN_TOKEN_LOG_DB = 'users_login_token_log';
+
+}
