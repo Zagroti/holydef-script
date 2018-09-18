18f2e31c5f60f45916b9b4bc4916ab9c203fdefe
yaser <yaser.darzi@gmail.com>
2018-09-18 13:48:57 +0430

    add article test seeder

--- database/seeds/TestTableSeeder.php
+++ database/seeds/TestTableSeeder.php
@@ -12,12 +12,27 @@ class TestTableSeeder extends Seeder
     public function run()
     {
         $faker = Faker\Factory::create();
-        for ($i = 0; $i <= 10; $i++)
+        for ($i = 1; $i <= 10; $i++)
             \App\Category::create([
                 "title" => $faker->title,
                 "description" => $faker->text,
                 "path" => $faker->imageUrl(),
                 "type_path" => 2
             ]);
+        for ($i = 0; $i <= 100; $i++)
+            \App\Article::create([
+                "cat_id" => rand(1, 10),
+                "title" => $faker->title,
+                "short_description" => $faker->text,
+                "description" => $faker->text,
+                "image" => $faker->imageUrl(),
+                "type_image" => 2,
+                "video" => null,
+                "type_video" => 0,
+                "audio" => null,
+                "type_audio" => 0,
+            ]);
+
+
     }
 }
