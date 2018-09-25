0e1ba53565bc5eb64459b6db48b5326eb3412e8b
yaser <yaser.darzi@gmail.com>
2018-09-25 15:08:02 +0330

    test table seeder edit

--- database/seeds/TestTableSeeder.php
+++ database/seeds/TestTableSeeder.php
@@ -26,19 +26,19 @@ class TestTableSeeder extends Seeder
                 "audio" => "https://www.irantunez.com/uploads/audios/1721f186dc9dc90fa4d972186bc636c7/dariush-shekanjeh-gar.mp3",
                 "type_audio" => 2,
             ]);
-        $faker = Faker\Factory::create();
-        for ($i = 0; $i <= 100; $i++)
-            \App\Article::create([
-                "cat_id" => rand(1, 12),
-                "title" => $faker->jobTitle,
-                "short_description" => $faker->text(100),
-                "description" => $faker->text(200),
-                "image" => $faker->imageUrl(),
-                "type_image" => 2,
-                "video" => "http://clips.vorwaerts-gmbh.de/VfE_html5.mp4",
-                "type_video" => 2,
-                "audio" => "https://www.irantunez.com/uploads/audios/1721f186dc9dc90fa4d972186bc636c7/dariush-shekanjeh-gar.mp3",
-                "type_audio" => 2,
-            ]);
+//        $faker = Faker\Factory::create();
+//        for ($i = 0; $i <= 100; $i++)
+//            \App\Article::create([
+//                "cat_id" => rand(1, 12),
+//                "title" => $faker->jobTitle,
+//                "short_description" => $faker->text(100),
+//                "description" => $faker->text(200),
+//                "image" => $faker->imageUrl(),
+//                "type_image" => 2,
+//                "video" => "http://clips.vorwaerts-gmbh.de/VfE_html5.mp4",
+//                "type_video" => 2,
+//                "audio" => "https://www.irantunez.com/uploads/audios/1721f186dc9dc90fa4d972186bc636c7/dariush-shekanjeh-gar.mp3",
+//                "type_audio" => 2,
+//            ]);
     }
 }
