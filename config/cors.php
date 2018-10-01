ad4b0aa62e39753d333019b01260c5868564c42f
yaser <yaser.darzi@gmail.com>
2018-10-01 12:38:06 +0330

    add cores

--- config/cors.php
+++ config/cors.php
@@ -16,7 +16,7 @@ return [
     'supportsCredentials' => false,
     'allowedOrigins' => ['*'],
     'allowedOriginsPatterns' => [],
-    'allowedHeaders' => ['Content-Type', 'Accept', 'X-Requested-With', 'Authorization', 'market', 'X-Debug'],
+    'allowedHeaders' => ['Content-Type', 'Accept', 'X-Requested-With', 'Authorization', 'market', 'X-Debug', 'Origin', 'Access-Control-Allow-Headers'],
     'allowedMethods' => ['*'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
     'exposedHeaders' => [],
     'maxAge' => 0,
