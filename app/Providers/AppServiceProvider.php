199838a99acfbefa3d837e35407955a493dd0e70
yaser <yaser.darzi@gmail.com>
2018-10-01 13:11:14 +0330

    add cores

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $request = app('request');
+        if ($request->isMethod('OPTIONS')) {
+            app()->options($request->path(), function () {
+                return response('', 200);
+            });
+        }
         //
     }
 }
