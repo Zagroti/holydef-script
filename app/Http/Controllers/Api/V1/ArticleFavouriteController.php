4345318b72cdc2bf75c94d36dbed5720b07db44f
yaser <yaser.darzi@gmail.com>
2018-10-01 18:19:17 +0330

    add article to index favourite

--- app/Http/Controllers/Api/V1/ArticleFavouriteController.php
+++ app/Http/Controllers/Api/V1/ArticleFavouriteController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api\V1;
 use App\ArticleFavourite;
 use App\Exceptions\ApiException;
 use App\Http\Controllers\ApiController;
+use App\Inside\Constants;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ArticleFavouriteController extends ApiController
 {
@@ -16,7 +18,21 @@ class ArticleFavouriteController extends ApiController
      */
     public function index(Request $request)
     {
-        $favourite = ArticleFavourite::where('user_id', $request->input('user_id'))->orderBy('created_at', 'DESC')->get();
+        $favourite = ArticleFavourite::join(Constants::ARTICLE_DB, Constants::ARTICLE_DB . ".id", "=", Constants::ARTICLE_FAVOURITE_DB . ".article_id")
+            ->select(
+                Constants::ARTICLE_DB . ".id",
+                Constants::ARTICLE_DB . ".cat_id",
+                Constants::ARTICLE_DB . ".title",
+                Constants::ARTICLE_DB . ".short_description",
+                DB::raw("CASE WHEN type_image = '2' THEN image WHEN image != '' THEN (concat ( '" . $request->root() . "/files/article/image/" . "', image) ) ELSE '' END as image"),
+                Constants::ARTICLE_DB . ".type_image",
+                DB::raw("CASE WHEN type_video = '2' THEN video WHEN video != '' THEN (concat ( '" . $request->root() . "/files/article/video/" . "', video) ) ELSE '' END as video"),
+                Constants::ARTICLE_DB . ".type_video",
+                DB::raw("CASE WHEN type_audio = '2' THEN audio WHEN audio != '' THEN (concat ( '" . $request->root() . "/files/article/audio/" . "', audio) ) ELSE '' END as audio"),
+                Constants::ARTICLE_DB . ".type_audio",
+                Constants::ARTICLE_FAVOURITE_DB . ".*"
+            )
+            ->where(Constants::ARTICLE_FAVOURITE_DB . '.user_id', $request->input('user_id'))->orderBy(Constants::ARTICLE_FAVOURITE_DB . '.created_at', 'DESC')->get();
         return $this->respond($favourite);
     }
 
