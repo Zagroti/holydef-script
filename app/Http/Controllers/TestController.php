81eafdf700169f69f5d68d72fee639dea0aad3e8
yaser <yaser.darzi@gmail.com>
2018-10-16 13:00:33 +0330

    test api for vas

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function test(Request $request)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://217.218.113.22:8090/v1/subscribe?service_id=9823&user_number=09111160804");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: */*'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec($ch);
+        $server_output = json_decode($server_output, true);
+        dd($ch);
+        curl_close($ch);
+    }
+}
