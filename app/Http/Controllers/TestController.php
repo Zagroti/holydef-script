138d3fe61a89e9b1fb8353e45b201f539e16d1a2
yaser <yaser.darzi@gmail.com>
2018-10-16 13:35:53 +0330

    test api for vas

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,7 +9,7 @@ class TestController extends Controller
     public function test(Request $request)
     {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "http://217.218.113.22:8090/v1/subscribe?service_id=9823&user_number=09111160804&product_id=209623");
+        curl_setopt($ch, CURLOPT_URL, "http://217.218.113.22:8090/v1/subscribe?service_id=9823&user_number=09111160804&product_id=e52d6162eed64693a676cca8fdfb1e8b");
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: */*'));
