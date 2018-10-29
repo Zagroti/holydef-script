1d8d45e30c68e677901397c6ab637f49e3882089
yaser <yaser.darzi@gmail.com>
2018-10-29 15:40:00 +0330

    check status

--- app/Console/Commands/CheckStatus.php
+++ app/Console/Commands/CheckStatus.php
@@ -39,20 +39,21 @@ class CheckStatus extends Command
     public function handle()
     {
         $users = User::join("users_token", "users.id", "=", "users_token.user_id")->get();
-        dd($users );
-        if ($users)
-            foreach ($users as $value) {
-                echo $value->phone . "\n";
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, "http://" . env('VAS_IP') . ":" . env('VAS_PORT') . "/v1/charge/status?‬‬?token=" . $value->token);
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: */*'));
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                $server_output = curl_exec($ch);
-                $server_output = json_decode($server_output, true);
-                curl_close($ch);
-                print_r($server_output);
-            }
+        echo "count= " . sizeof($users) . "\n";
+        foreach ($users as $value) {
+            echo "phone= " . $value->phone . "\n";
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, "http://" . env('VAS_IP') . ":" . env('VAS_PORT') . "/v1/charge/status?‬‬?token=" . $value->token);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: */*'));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $server_output = curl_exec($ch);
+            $server_output = json_decode($server_output, true);
+            curl_close($ch);
+            echo "status= ";
+            print_r($server_output);
+            echo "\n";
+        }
     }
 }
