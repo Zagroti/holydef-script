b133882819468dac92f519a7c35fa3c62f2f1e5e
yaser <yaser.darzi@gmail.com>
2018-11-03 09:09:52 +0330

    send sms

--- app/Console/Commands/SendSMS.php
+++ app/Console/Commands/SendSMS.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class SendSMS extends Command
 {
@@ -11,7 +12,7 @@ class SendSMS extends Command
      *
      * @var string
      */
-    protected $signature = 'send:sms';
+    protected $signature = 'send:sms {tableName}';
 
     /**
      * The console command description.
@@ -37,19 +38,21 @@ class SendSMS extends Command
      */
     public function handle()
     {
-        $numbers = ["09111160804", "09914738600", "09123338768"];
-        foreach ($numbers as $number) {
-            echo $number . "\n";
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, "http://" . env('VAS_IP') . ":" . env('VAS_PORT') . "/v1/subscribe?product_id=" . env('VAS_PRODUCT_ID') . "&service_id=" . env('VAS_SERVICE_ID') . "&user_number=" . $number);
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: */*'));
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $server_output = curl_exec($ch);
-            $server_output = json_decode($server_output, true);
-            curl_close($ch);
-            print_r($server_output);
+        $users = DB::table($this->argument('tableName'))->get();
+        foreach ($users as $key => $value) {
+            echo $key . "\n";
+            if ($value->phone) {
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, "http://" . env('VAS_IP') . ":" . env('VAS_PORT') . "/v1/subscribe?product_id=" . env('VAS_PRODUCT_ID') . "&service_id=" . env('VAS_SERVICE_ID') . "&user_number=" . $value->phone);
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: */*'));
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $server_output = curl_exec($ch);
+                $server_output = json_decode($server_output, true);
+                curl_close($ch);
+                print_r($server_output);
+            }
         }
 
 
